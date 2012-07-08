7b6a617065e3f314b13aa5a8fad44774a21f34f1
Adrian Dudau <adrian@vermisoft.ro>
2012-07-08 23:37:23 +0200

    Complete and tested version of the LE and RE. Ready for midterm evaluation

--- wiselib.testing/algorithms/routing/ctp/ctp_debugging.h
+++ wiselib.testing/algorithms/routing/ctp/ctp_debugging.h
@@ -21,30 +21,68 @@
  * Author: Adrian Dudau <adrian@vermisoft.ro>
  */
 
-#ifndef CTP_TYPES_H_
-#define CTP_TYPES_H_
+#ifndef CTP_TESTING_H_
+#define CTP_TESTING_H_
 
 #include "util/serialization/simple_types.h"
 
-namespace wiselib
-{
+//uncomment for debugging mode
+#define CTP_DEBUGGING
 
-   typedef uint8_t ctp_msg_options_t;
-   typedef uint16_t ctp_msg_etx_t;
+//Uncomment to enable general RE debug messages
+//#define ROUTING_ENGINE_DEBUG
 
-//   enum {
-//       SUCCESS = 1,
-//       FAIL = 2,
-//       EBUSY = 3,
-//       ERETRY = 4,
-//       ECANCEL = 5,
-//       EOFF = 6,
-//       ESIZE = 7
-//   } error_t;
+//Uncomment to enable general LE debug messages
+//#define LINK_ESTIMATOR_DEBUG
 
-   typedef int error_t;
+#define NODES_NR												8
+//#define NODES														{410,411,412,413,414,415,416,417} //tubs isense ids
+#define NODES														{0,1,2,3,4,5,6,7} //Shawn ids
+
+/* All these numbers represent indexes of the node IDs in the NODES list. */
+
+#define CONNECTIONS_NR									8
+#define CONNECTIONS 								{{0,1},{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,0}}
+
+//Nodes acting as roots/sinks
+#define ROOT_NODES_NR 									1
+#define ROOT_NODES 											{6}
+
+//Node indexes we want to print debug messages on
+#define DEBUG_NODES										{0,1,2,3,4,5,6,7}
+#define DEBUG_NODES_NR 									8
+
+namespace wiselib {
+
+typedef struct {
+	uint16_t n1;
+	uint16_t n2;
+} connections_t;
+
+const connections_t connections[CONNECTIONS_NR] = CONNECTIONS;
+const uint16_t nodes[NODES_NR] = NODES;
+const uint16_t root_nodes[ROOT_NODES_NR] = ROOT_NODES;
+const uint16_t debug_nodes[DEBUG_NODES_NR] = DEBUG_NODES;
+
+typedef int deb;
+
+bool areConnected(uint16_t n1, uint16_t n2) {
+
+	int i;
+
+	for (i = 0; i < CONNECTIONS_NR; i++) {
+		if (((n1 == nodes[connections[i].n1])
+				&& (n2 == nodes[connections[i].n2]))
+				|| ((n1 == nodes[connections[i].n2])
+						&& (n2 == nodes[connections[i].n1]))) {
+			return true;
+		}
+	}
+
+	return false;
 
 }
 
+}
 
 #endif /* CTP_TYPES_H_ */
